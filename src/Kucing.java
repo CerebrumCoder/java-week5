cf0400a54c8105e9c491d5c21c7eb7a7b4ab9e20
Neal Guarddin <neal@Neals-Air.lan>
2025-02-24 09:21:41 +0700

    Update pertama

---
+++ src/Kucing.java
@@ -0,0 +1,22 @@
+public class Kucing {
+    // Atribut
+    private String nama;
+    private int umur;
+
+    // Method
+    public String getNama() {
+        return nama;
+    }
+
+    public int getUmur() {
+        return umur;
+    } 
+
+    public void setUmur(int umr) {
+        umur = umr;
+    }
+
+    public void setNama(String nm) {
+        nama = nm;
+    }
+}
